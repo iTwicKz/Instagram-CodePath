6da6dd364e501e1340b01842cdcb4e1d6aa0db38
iTwicKz <twickes32@ufl.edu>
2016-01-07 19:23:28 -0500

    Initial Commit

---
+++ Instagram-Clone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram-Clone
+//
+//  Created by Takashi Wickes on 1/7/16.
+//  Copyright © 2016 Takashi Wickes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
